db53986b405fc49a56f9b98b20b8c1dbcbe88412
SivaldoMuniz <sivaldo5@hotmail.com>
2019-01-07 00:15:30 -0200

    Atualização do Projeto Xadrez

---
+++ xadrez-console/xadrez-console/Tela.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i=0; i<tab.linhas; i++){
+                for (int  j=0; j<tab.linhas; j++)
+                {
+                    Console.Write(tab.peca(i,j) + " ");
+                    Console.Write("- ");
+                }
+                Console.WriteLine();
+                 
+            }
+        }
+    }
+}
